a88b0e53b34abcf324a64d4e216afc630796845b
alazareva <a.lazareva@gmail.com>
2020-08-06 19:34:32 -0700

    working cube

--- cube.js
+++ cube.js
@@ -1,45 +1,120 @@
+class Face {
+    normal;
+    c;
 
+    constructor(normal, c){
+        this.normal = normal;
+        this.c = c;
+    }
+
+    turnZ(angle){
+        let v2 = createVector(
+            round(this.normal.x * cos(angle) - this.normal.y * sin(angle)),
+            round(this.normal.x * sin(angle) + this.normal.y * cos(angle)),
+            round(this.normal.z)
+        );
+        this.normal = v2;
+    }
+
+
+    turnY(angle){
+        let v2 = createVector(
+            round(this.normal.x * cos(angle) - this.normal.z * sin(angle)),
+            round(this.normal.y),
+            round(this.normal.x * sin(angle) + this.normal.z * cos(angle)),
+
+        );
+        this.normal = v2;
+    }
+
+    turnX(angle){
+        let v2 = createVector(
+            round(this.normal.x),
+            round(this.normal.y * cos(angle) - this.normal.z * sin(angle)),
+            round(this.normal.y * sin(angle) + this.normal.z * cos(angle)),
+
+        );
+        this.normal = v2;
+    }
+
+    show(){
+        push();
+        fill(this.c);
+        noStroke();
+        rectMode(CENTER);
+        translate(0.5*this.normal.x, 0.5*this.normal.y, 0.5*this.normal.z);
+        if (abs(this.normal.x) > 0) {
+            rotateY(HALF_PI);
+          } else if (abs(this.normal.y) > 0) {
+            rotateX(HALF_PI);
+          }
+          square(0, 0, 1);
+          pop();
+    }
+}
 class Cubie {
     matrix;
-    highlight = false;
     x;
     y;
     z;
-    fff;
+    c;
+    faces;
+
     constructor(matrix, x, y, z) {
         this.matrix = matrix;
         this.x = x;
         this.y = y;
         this.z = z;
-        this.fff = random(255);
+
+        let black = color(0, 0, 0);
+
+        this.faces = [
+            new Face(createVector(0, 0, -1), z == -1? color('#FF0092'): color(0, 0, 0)),
+            new Face(createVector(0, 0,  1), z == 1? color('#FFCA1B'): color(0, 0, 0)),
+            new Face(createVector(0, 1,  0), y == 1? color(255, 255, 255): color(0, 0, 0)),
+            new Face(createVector(0, -1,  0), y == -1? color('#B6FF00'): color(0, 0, 0)),
+            new Face(createVector(1, 0,  0), x == 1? color('#228DFF'): color(0, 0, 0)),
+            new Face(createVector(-1, 0,  0), x == -1 ? color('#BA01FF'): color(0, 0, 0)),
+        ]
     }
 
-    set high(h) {
-        this.highlight = h;
-      }
+    turnFacesZ(dir){
+        for (let f of this.faces){
+            f.turnZ(dir * HALF_PI);
+        }
+    }
+
+    turnFacesY(dir){
+        for (let f of this.faces){
+            f.turnY(dir * HALF_PI);
+        }
+    }
+
+
+    turnFacesX(dir){
+        for (let f of this.faces){
+            f.turnX(dir * HALF_PI);
+        }
+    }
 
     update(_x,  _y, _z){
         this.matrix = new p5.Matrix('mat4');
         this.matrix.translate([_x, _y, _z]);
-        console.log(_x);
-        console.log(_y);
-        console.log(_z);
-        //console.log(this.matrix);
         this.x = _x;
         this.y = _y;
         this.z = _z;
     }
 
     show() {
-        fill(this.fff);
-        if (this.highlight) {
-            fill(50);
-        }
+        noFill();
         stroke(0);
         strokeWeight(1);
         push();
         applyMatrix(...this.matrix.mat4);
         box(1);
+        for (let f of this.faces){
+            f.show()
+        }
         pop();
     }
 
--- sketch.js
+++ sketch.js
@@ -3,8 +3,34 @@ var x = 0;
 var y = 0;
 var z = 0;
 
+var current_move;
+
+speed = 0.5;
+
+allMoves = [
+    new Move(0, 1, 0, 1), 
+    new Move(0, 1, 0, -1), 
+    new Move(0, -1, 0, 1), 
+    new Move(0, -1, 0, -1), 
+    new Move(1, 0, 0, 1), 
+    new Move(1, 0, 0, -1), 
+    new Move(-1, 0, 0, 1), 
+    new Move(-1, 0, 0, -1), 
+    new Move(0, 0, 1, 1), 
+    new Move(0, 0, 1, -1), 
+    new Move(0, 0, -1, 1), 
+    new Move(0, 0, -1, -1) 
+];
+
+sequence = [];
+counter = 0;
+started = false;
+  
+
 function setup() {
     createCanvas(710, 400, WEBGL);
+    createEasyCam();
+    document.oncontextmenu = ()=>false;
     for (let x = -1; x <= 1; x++) {
         for (let y = -1; y <= 1; y++) {
             for (let z = -1; z <= 1; z ++) {
@@ -14,30 +40,22 @@ function setup() {
             }
         }
      }
-     cube[0].high = true;
-     //turnZ();
      smooth();
-}
 
-function keyPressed(){
-    turnZ(0, 1);
-}
-/*
-void turnZ(int index, int dir) {
-  for (int i = 0; i < cube.length; i++) {
-    Cubie qb = cube[i];
-    if (qb.z == index) {
-      PMatrix2D matrix = new PMatrix2D();
-      matrix.rotate(dir*HALF_PI);
-      matrix.translate(qb.x, qb.y);
-      matrix.print();
-      qb.update(round(matrix.m02), round(matrix.m12), round(qb.z));
-      qb.turnFacesZ(dir);
-    }
-  }
-}
-*/
+     for (let b = 0; b < 10; b++) {
+         let r = int(random(allMoves.length));
+         let m = allMoves[r];
+         sequence.push(m);
+     }
 
+    // current_move = sequence[counter];
+     for (i = sequence.length - 1; i >= 0; i --){
+         next_move = sequence[i].copy();
+         next_move.reverse()
+         sequence.push(next_move);
+     }
+     // current_move.start(); 
+}
 
 function rotate2d(matrix, angle) {
     s = sin(angle);
@@ -70,38 +88,160 @@ function new_2d_mat(){
              0.0000,  1.0000,  0.0000]
 }
 
-function turnZ(index, dir){
+
+function keyPressed() {
+    animate_move(key);
+  }
+
+  function turnZ(index, dir){
     for (let c of cube) {
         if (c.z == index) {
             m2d = new_2d_mat();
-            rotate2d(m2d, HALF_PI);
+            rotate2d(m2d, dir*HALF_PI);
             translate2d(m2d, c.x, c.y)
-            console.log('cx', c.x)
-            console.log('cy', c.y)
-            console.log(m2d)
             c.update(round(m2d[2]), round(m2d[5]), c.z);
+            c.turnFacesZ(dir);
         }
+    }
+}
+
+
+function turnY(index, dir) {
+    for (let c of cube) {
+      if (c.y == index) {
+        m2d = new_2d_mat();
+        rotate2d(m2d, dir*HALF_PI);
+        translate2d(m2d, c.x, c.z);
+        c.update(round(m2d[2]), c.y, round(m2d[5]));
+        c.turnFacesY(dir);
+      }
+    }
+  }
+
+  function turnX(index, dir) {
+    for (let c of cube) {
+      if (c.x == index) {
+        m2d = new_2d_mat();
+        rotate2d(m2d, dir*HALF_PI);
+        translate2d(m2d, c.y, c.z);
+        c.update(c.x, round(m2d[2]), round(m2d[5]));
+        c.turnFacesX(dir);
+      }
+    }
+  }
+
+move_lookup = {
+    'u': new Move(0, 1, 0, 1), 
+    'U': new Move(0, 1, 0, -1), 
+    'f': new Move(0, 0, 1, 1), 
+    'F': new Move(0, 0, 1, -1),
+    'b':  new Move(0, 0, -1, 1),
+    'B':  new Move(0, 0, -1, -1),
+    'd':  new Move(0, -1, 0, 1),
+    'D':  new Move(0, -1, 0, -1),
+    'l':  new Move(-1, 0, 0, 1),
+    'L':  new Move(-1, 0, 0, -1),
+    'r':  new Move(1, 0, 0, 1),
+    'R':  new Move(1, 0, 0, -1),
+
+
+}
+
+function animate_move(move){
+    if (move in move_lookup & (!current_move || current_move.finished)) {
+        current_move = move_lookup[move]
+        current_move.start()
     }
 }
   
 
+function applyMove(move) {
+    switch (move) {
+    case 'f': 
+      turnZ(1, 1);
+      break;
+    case 'F': 
+      turnZ(1, -1);
+      break;  
+    case 'b': 
+      turnZ(-1, 1);
+      break;
+    case 'B': 
+      turnZ(-1, -1);
+      break;
+    case 'u': 
+      turnY(1, 1);
+      break;
+    case 'U': 
+      turnY(1, -1);
+      break;
+    case 'd': 
+      turnY(-1, 1);
+      break;
+    case 'D': 
+      turnY(-1, -1);
+      break;
+    case 'l': 
+      turnX(-1, 1);
+      break;
+    case 'L': 
+      turnX(-1, -1);
+      break;
+    case 'r': 
+      turnX(1, 1);
+      break;
+    case 'R': 
+      turnX(1, -1);
+      break;
+    }
+  }
+
+
 function draw(){
-    //rotateZ(frameCount * 0.01);
-    //rotateX(frameCount * 0.01);
-    //rotateY(frameCount * 0.01);
-       
+    background(360);
     let posX = width/2;
     let posY = height/2;
     let angle = Math.atan2(mouseY-posY, mouseX-posX);
-    rotateZ(angle);
-    rotateY(angle);
-    rotateX(angle);
+    rotateX(-0.5);
+    rotateY(0.4);
+    rotateZ(0.1);  
     //rotateX(frameCount * 0.01);
     //rotateY(frameCount * 0.01);
-    scale(30);
-    background(200);
-    for (let c of cube) {
-        c.show();
-    }
+   // rotateX(angle);
+    //rotateY(angle);
 
+    /*
+    console.log(current_move.finished)
+    */
+    if (current_move){
+        console.log('updating move', current_move.angle)
+        current_move.update();
+    }
+    /*
+    if (current_move.finished) {
+      if (counter < sequence.length-1) {
+        console.log('counter++')
+        counter++;
+        current_move = sequence[counter];
+        current_move.start();
+      }
+    }*/
+
+
+    scale(40);
+
+    for (i = 0; i < cube.length; i++) {
+        push();
+        if (current_move){
+        if (abs(cube[i].z) > 0 && cube[i].z == current_move.z) {
+          rotateZ(current_move.angle);
+        } else if (abs(cube[i].x) > 0 && cube[i].x == current_move.x) {
+          rotateX(current_move.angle);
+        } else if (abs(cube[i].y) > 0 && cube[i].y == current_move.y) {
+          rotateY(-current_move.angle);
+        }
+    }   
+    cube[i].show();
+        pop();
+}
 }
